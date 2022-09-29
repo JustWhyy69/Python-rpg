17561faa78f2e6d712fd82b19e03ada745f95e37
JustWhyy69 <kayla.merchant10@gmail.com>
2022-09-29 13:51:16 -0400

    rpgBirthday

---
+++ rpg/armor.py
@@ -0,0 +1,6 @@
+class Armor():
+    def _init_(self):
+        self.name = "No Armor"
+        self.price = 0
+        self.weight = 0
+        self.ac = 11
---
+++ rpg/character.py
@@ -0,0 +1,10 @@
+class Character():
+    def _init_(self):
+        self.name = "player"
+        self.strength = 0
+    
+    def set_current_weapon(self):
+        pass
+
+    def roll_to_hit(self):
+        pass
